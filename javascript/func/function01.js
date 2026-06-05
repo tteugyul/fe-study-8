7fdb3183189c8d280ff2e55d6daa34c35e899dc9
tteugyul <yZQGCY0oHy0WrijW6cL7HQiHu2BKtt@gmail.com>
2026-06-05 15:48:34 +0900

    javascript 함수, 콜백함수

---
+++ javascript/func/function01.js
@@ -0,0 +1,21 @@
+// function 함수
+
+/*
+
+    function 함수명(매개변수){
+    
+        실행코드;
+        return 리턴값;
+        
+    }
+
+*/
+
+function func01() {
+
+    console.log("function01()");
+
+}
+
+func01(); // 함수호출
+
